e20e487dc0347458150d488a3cd0f497fd573d7f
조수아 <suacho0724@gmail.com>
2024-03-28 10:27:44 +0900

    🎉 Start: back 개발 시작 기본 파일 upload

---
+++ src/test/java/com/jmdm/squiz/SquizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmdm.squiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
