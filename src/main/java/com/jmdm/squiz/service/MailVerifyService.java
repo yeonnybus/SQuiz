8d15e8bc05d0b3931398247d75efc9a2159e6a39
조수아 <suacho0724@gmail.com>
2024-04-18 09:55:07 +0900

    🚚 Rename: 인증 코드 관련 파일 명 수정
    [PP2024-171] CertificationNumberDao -> RedisService

--- src/main/java/com/jmdm/squiz/service/MailVerifyService.java
+++ src/main/java/com/jmdm/squiz/service/MailVerifyService.java
@@ -4,27 +4,26 @@ import com.jmdm.squiz.exception.ErrorCode;
 import com.jmdm.squiz.exception.model.NotFoundEmailException;
 import com.jmdm.squiz.exception.model.WrongCertificationNumException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class MailVerifyService {
-    private final CertificationNumberDao certificationNumberDao;
+    private final RedisService redisService;
 
     public void verifyEmail(String email, String certificationNumber) {
         if (!isVerify(email, certificationNumber)) {
             throw new WrongCertificationNumException(ErrorCode.CERTIFICATION_NUM_ERROR, ErrorCode.CERTIFICATION_NUM_ERROR.getMessage());
         }
-        certificationNumberDao.deleteData(email);
+        redisService.deleteData(email);
     }
 
     private boolean isVerify(String email, String certificationNumber) {
-        String result = certificationNumberDao.getData(email);
+        String result = (String)redisService.getData(email);
         if(result == null){
             throw new NotFoundEmailException(ErrorCode.EMAIL_NOT_FOUND, ErrorCode.EMAIL_NOT_FOUND.getMessage());
         }
-        return certificationNumberDao.getData(email).equals(certificationNumber);
+        return result.equals(certificationNumber);
 //        boolean validatedEmail = isEmailExists(email);
 //        System.out.println("validatedEmail = " + validatedEmail);
 //        if (!validatedEmail) {
